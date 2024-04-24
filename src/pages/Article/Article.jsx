9aca3698a642fda61a20a7849f14dafe959213a0
vbegunova <veronikabegunova42@gmail.com>
2024-04-24 12:58:59 -0500

    Update Article.jsx

--- src/pages/Article/Article.jsx
+++ src/pages/Article/Article.jsx
@@ -16,28 +16,26 @@ const Article = () => {
   const { articleId } = useParams();
   const [data, setData] = useState({});
 
-  // Создаем функцию fetchArticle, которая будет загружать данные из локального файла JSON или массива blog
-  const fetchArticle = async () => {
-    try {
-      let articleData;
-      if (articleId) {
-        const article = blog.find(item => item.index === parseInt(articleId));
-        articleData = article || {};
-      } else {
-        articleData = {};
-      }
-      setData(articleData);
-    } catch (error) {
-      console.error('Error fetching article:', error);
-    }
-  };
-
-  const { title, text, date, bgImg } = data;
-
-  // Вызываем функцию fetchArticle при загрузке компонента или при изменении movieId
   useEffect(() => {
+    const fetchArticle = async () => {
+      try {
+        let articleData;
+        if (articleId) {
+          const article = blog.find(item => item.index === parseInt(articleId));
+          articleData = article || {};
+        } else {
+          articleData = {};
+        }
+        setData(articleData);
+      } catch (error) {
+        console.error('Error fetching article:', error);
+      }
+    };
+
     fetchArticle();
   }, [articleId]);
+
+  const { title, text, date, bgImg } = data;
 
   return (
     <>
